244716de4245badfa06d090f50595af469bb7c7b
Axmjai <152575142+Axmjai@users.noreply.github.com>
2024-01-05 10:57:11 +0700

    Work4
    Work4

---
+++ post.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST</title>
+</head>
+<body>
+<h1 style="text-align: center;">Wedboard Suphakorn</h1>
+    <hr>   
+<div style="text-align: center;">
+    <?php
+            echo "ต้องการดูกระทู้หมายเลข $_GET[id]<br>";
+    ?>  
+     
+</div>
+<table style="border:2px solid black;width: 40%;" align="center">
+      <tr><td colspan="2" style="background-color: #6cd2fe;">เเสดงความคิดเห็น</td></tr><br>
+       <tr><td colspan="1"><textarea name="MS" rows="20%" cols="100%">
+        </textarea>
+        <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"> </td></tr>
+    </td></tr> 
+        
+
+      </table>
+      <div style="text-align: center;">
+      <br>
+                 <a href="index.html">กลับไปหน้าหลัก</a>
+            </div>
+      <Form>
+        
+    </Form>
+</body>
+</html>
